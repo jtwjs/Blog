bebcc9ed3f4cd3afe51cb058fc3d5cab49a9307a
jtwjs <xodnd9503@gmail.com>
2023-05-08 20:40:21 +0900

    :sparkles: feat: add typography component and story (#1)

---
+++ packages/ui/src/typography/Typography.stories.tsx
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Typography } from "./Typography.component";
+
+const meta: Meta = {
+  title: "Foundation/Typography",
+  component: Typography,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Typography>;
+
+export const Heading: Story = {
+  args: {
+    variant: "h3",
+    weight: "regular",
+  },
+  parameters: {
+    controls: { exclude: ["as", "className", "variant", "weight"] },
+  },
+  render: (args) => (
+    <>
+      <Typography {...args}>Heading 3</Typography>
+      <Typography {...args} variant="h2">
+        Heading 2
+      </Typography>
+      <Typography {...args} variant="h1">
+        Heading 1
+      </Typography>
+    </>
+  ),
+};
+
+export const Text: Story = {
+  args: {
+    variant: "xs",
+    weight: "regular",
+  },
+  parameters: {
+    controls: { exclude: ["as", "className"] },
+  },
+  render: (args) => (
+    <>
+      <Typography {...args}>인생은 속도보다 방향이다.</Typography>
+      <Typography {...args} variant="sm">
+        인생은 속도보다 방향이다.
+      </Typography>
+      <Typography {...args} variant="md">
+        인생은 속도보다 방향이다.
+      </Typography>
+      <Typography {...args} variant="lg">
+        인생은 속도보다 방향이다.
+      </Typography>
+      <Typography {...args} variant="xl">
+        인생은 속도보다 방향이다.
+      </Typography>
+    </>
+  ),
+};
