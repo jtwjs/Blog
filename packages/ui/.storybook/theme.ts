e4a293365c8c78c9f813e1c2a0ad978473403a45
jtwjs <xodnd9503@gmail.com>
2023-05-08 20:38:05 +0900

    :sparkles: feat: add design system package (#1)

---
+++ packages/ui/.storybook/theme.ts
@@ -0,0 +1,21 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'jtw Blog UI',
+  colorPrimary: '#3F4150',
+  colorSecondary: '#3DA5F5',
+  appBg: '#ffffff',
+  appContentBg: '#ffffff',
+  appBorderColor: '#3F4150',
+  appBorderRadius: 4,
+  textColor: '#191A20',
+  textInverseColor: '#ffffff',
+  barTextColor: '#3F4150',
+  barSelectedColor: '#3F4150',
+  barBg: '#ffffff',
+  inputBg: '#ffffff',
+  inputBorder: '#E0E2E7',
+  inputTextColor: '#191A20',
+  inputBorderRadius: 2,
+});
---
+++ packages/ui/tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*"],
+  darkMode: ["class", '[data-mode="dark"]'],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        black: "#191A20",
+        primary: "#3F4150",
+        secondary: "#8C8D96",
+        tertiary: "#B2B3B9",
+        border: "#E0E2E7",
+        background: "#F7F8FA",
+        white: "#ffffff",
+        brand: "#3DA5F5",
+        tint: "#ECF6FE",
+        shade: "#3186C4",
+        red: "#F86D7D",
+        green: "#22C58B",
+      },
+      fontSize: {
+        xxs: ["12px", "1.3333"],
+        xs: ["13px", "1.5384"],
+        sm: ["14px", "1.7142"],
+        md: ["16px", "1.5"],
+        lg: ["18px", "1.5555"],
+        xl: ["24px", "1.4167"],
+        h3: ["48px", "1.25"],
+        h2: ["60px", "1.2"],
+        h1: ["72px", "1.25"],
+      },
+      screens: {
+        md: "768px",
+        lg: "1200px",
+      },
+    },
+  },
+  plugins: [],
+};
