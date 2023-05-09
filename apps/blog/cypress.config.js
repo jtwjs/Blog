037aa4f113700eab15d9f97e9ab838db9cb64f0b
jtwjs <xodnd9503@gmail.com>
2023-05-09 15:48:47 +0900

    :sparkles: feat: set up e2e tool cypress (#3)

---
+++ apps/blog/cypress.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
