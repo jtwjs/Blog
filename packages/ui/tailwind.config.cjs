74fde83adfa8814e95b0b8323dbf7fe4b3f20779
jtwjs <xodnd9503@gmail.com>
2023-05-09 15:54:22 +0900

    :building_construction: feat: change dir structure (#1)

--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   darkMode: ["class", '[data-mode="dark"]'],
   theme: {
     extend: {
