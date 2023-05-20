eb11ec296ce9e124e6a729924ec220b7b08dd5ba
Taewoong Jung <xodnd9503@gmail.com>
2023-05-20 19:05:03 +0900

    Feature/#12 storybook (#13)
    * :sparkles: add dark mode switch component with story (#12)
    
    * :sparkles: add z-index token (#12)
    
    * :lipstick: add rounded

---
+++ packages/ui/src/atom/switch/darkmode/DarkModeSwitch.stories.tsx
@@ -0,0 +1,48 @@
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+
+import DarkModeSwitch, {
+  type DarkModeSwitchProps,
+} from "./DarkModeSwitch.component";
+
+const meta: Meta<typeof DarkModeSwitch> = {
+  title: "Switch/DarkMode",
+  component: DarkModeSwitch,
+  tags: ["autodocs"],
+  parameters: {
+    controls: {
+      exclude: ["onToggle"],
+    },
+    componentSource: {
+      code: 'export default "This is my code"',
+      language: "typescript",
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DarkModeSwitch>;
+
+const StoryDarkModeSwitch = (args: DarkModeSwitchProps) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleDarkModeToggle = () => {
+    setIsDarkMode((prev) => !prev);
+    action("toggle dark mode");
+  };
+
+  return (
+    <DarkModeSwitch
+      {...args}
+      isDarkMode={isDarkMode}
+      onToggle={handleDarkModeToggle}
+    />
+  );
+};
+
+export const Default: Story = {
+  args: {},
+  render: (args) => StoryDarkModeSwitch(args),
+};
