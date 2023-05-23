50c3eeb873811670488f554a3676d4bc53b712fa
jtwjs <xodnd9503@gmail.com>
2023-05-23 12:56:36 +0900

    :sparkles: implement atoms and custom hook for mobil navbar

---
+++ apps/blog/src/components/layout/header/hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useMobileNavbar } from "./useMobileNavbar";
---
+++ apps/blog/src/components/layout/header/hooks/useMobileNavbar.ts
@@ -0,0 +1,25 @@
+import { useFixedBodyEl } from "@jtwjs/hooks";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+import { useShowMobileNav } from "@/store";
+
+interface UseMobileNavbar {
+  isShowMobileNav: boolean;
+  handleToggleMobileNav: () => void;
+}
+
+export default function useMobileNavbar(): UseMobileNavbar {
+  const pathname = usePathname();
+
+  const { isShowMobileNav, handleCloseMobileNav, handleToggleMobileNav } =
+    useShowMobileNav();
+
+  useFixedBodyEl(isShowMobileNav);
+
+  useEffect(() => {
+    handleCloseMobileNav();
+  }, [pathname, handleCloseMobileNav]);
+
+  return { isShowMobileNav, handleToggleMobileNav };
+}
