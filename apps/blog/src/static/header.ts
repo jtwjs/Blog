16c17ef7b3b0cdc88ae4b8e3b21d831dd2386bcc
jtwjs <xodnd9503@gmail.com>
2023-05-23 12:54:09 +0900

    :sparkles: add static modules for footer, header

---
+++ apps/blog/src/static/header.ts
@@ -0,0 +1,9 @@
+import { PATH } from "@/constants/path";
+
+export const NAV_MENU_LIST = [
+  { label: "Home", path: PATH.HOME },
+  { label: "Learn", path: PATH.LEARN },
+  { label: "Posts", path: PATH.POSTS },
+  { label: "Project", path: PATH.PROJECT },
+  { label: "About", path: PATH.ABOUT },
+];
