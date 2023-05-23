d6abc6518a0260f617087910723df83ae3ab95f3
jtwjs <xodnd9503@gmail.com>
2023-05-23 12:54:09 +0900

    :sparkles: add static modules for footer, header

---
+++ apps/blog/src/static/footer.ts
@@ -0,0 +1,25 @@
+import {
+  AiOutlineGithub,
+  AiOutlineInstagram,
+  AiOutlineMail,
+} from "react-icons/ai";
+
+export const COPYRIGHT = "ⓒ 2023 JTWJS BLOG. All Rights Reserved.";
+
+export const OUTBOUNDS = [
+  {
+    label: "Github",
+    link: "https://www.github.com/jtwjs",
+    Icon: AiOutlineGithub,
+  },
+  {
+    label: "Insta",
+    link: "https://www.instagram.com/jtwjs",
+    Icon: AiOutlineInstagram,
+  },
+  {
+    label: "Gmail",
+    link: "mailto:xodnd9503@gmail.com",
+    Icon: AiOutlineMail,
+  },
+];
